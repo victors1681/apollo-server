68fe15fb089c7c36163254036b88bba9bb80780b
Victor Santos <v.santos@itsoluclick.com>
2019-02-25 23:37:40 -0500

    authentication completed

--- src/index.js
+++ src/index.js
@@ -2,18 +2,33 @@ const { createStore } = require("./utils");
 const resolvers = require("./resolvers");
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
+const isEmail = require("isemail");
 
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
 
 const store = createStore();
+
+const context = async ({ req }) => {
+  const auth = (req.headers && req.headers.authorization) || "";
+  const email = Buffer.from(auth, "base64").toString("ascii");
+
+  if (!isEmail.validate(email)) return { user: null };
+
+  const users = await store.users.findOrCreate({ where: { email } });
+  const user = users && users[0] ? users[0] : null;
+
+  return { user: { ...user.dataValues } };
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
-  })
+  }),
+  context
 });
 
 server
